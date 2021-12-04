5403c6cc61a62eb37bbd4338c62b8f3aa4b7a0af
SheldyaewSergey <sheldyaew.sergey@mail.ru>
2021-12-04 15:44:37 +0300

    5

--- PZ_7/PZ_7.1.py
+++ PZ_7/PZ_7.1.py
@@ -1,1 +1,13 @@
 #1. Даны строки S и S0. Найти количество вхождений строки S0 в строку S.
+
+
+s = "1 1 2 3 5 6 5 4 5"
+s0 = "5"
+
+count = 0
+for a in s:
+    if a == s0:
+        count += 1
+    else:
+        continue
+print(count)
