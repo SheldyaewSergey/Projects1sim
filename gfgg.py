4a97fd7eed5ecc45b6efa07e63de4a4614be98af
SheldyaewSergey <tx_terminator@mail.ru>
2021-12-21 23:47:14 +0300

    10

--- gfgg.py
+++ gfgg.py
@@ -1,13 +1,3 @@
-k = int(input("Введите кол во чисел"))
-def input_convert_number():
-    while True:
-        try:
-            num = int(input("»>"))
-            break
-        except ValueError:
-            print("Надо ввести число")
-    return num
-numbers = []
-for _ in range(k):
-    numbers.append(input_convert_number())
-print(sum(numbers)/k)
+dictionary = []
+
+dictionary[word] = value
