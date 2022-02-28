c431b8bb94362070f6b42ae28660ddf395a50a1d
SheldyaewSergey <sheldyaew.sergey@mail.ru>
2022-02-28 19:18:57 +0300

    5

---
+++ PZ_13/PZ_13.2.py
@@ -0,0 +1,3 @@
+# 2. Из заданной строки отобразить только символы нижнего регистра.
+# Использовать библиотеку string. Строка 'In PyCharm, you can specify third-party standalone
+# applications and run them as External Tools'.
--- gfgg.py
+++ gfgg.py
@@ -1,3 +1,13 @@
-dictionary = []
-
-dictionary[word] = value
+k = int(input("Введите кол во чисел"))
+def input_convert_number():
+    while True:
+        try:
+            num = int(input("»>"))
+            break
+        except ValueError:
+            print("Надо ввести число")
+    return num
+numbers = []
+for _ in range(k):
+    numbers.append(input_convert_number())
+print(sum(numbers)/k)
