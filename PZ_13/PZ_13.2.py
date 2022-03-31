1821f0d2884d87a467d8afe353a4131a86951fc6
SheldyaewSergey <tx_terminator@mail.ru>
2022-03-31 18:27:50 +0300

    8

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -1,3 +1,14 @@
 # 2. Из заданной строки отобразить только символы нижнего регистра.
 # Использовать библиотеку string. Строка 'In PyCharm, you can specify third-party standalone
 # applications and run them as External Tools'.
+import string
+
+a = 'In PyCharm, you can specify third-party standalone ' \
+    'applications and run them as External Tools'
+b = string.ascii_lowercase
+
+for i in a:
+    if i in b:
+        print(i)
+    else:
+        continue
